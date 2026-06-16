126af529e1bfc624b9b94bdced9f66923fd8276a
KEYUR .D.PRAJAPATI <kkeyur136@gmail.com>
2026-06-16 07:48:08 +0000

    python with KEYZ

---
+++ keyzmain.py
@@ -0,0 +1,52 @@
+print("hello world") 
+# next vaeriable print 
+
+x="hello keyZ"
+print (x)
+## integer##
+age = 22
+print(age)
+
+## float ##
+
+a= 22.5
+print(a)
+
+## string char###
+name = "keyz"
+print(name)
+
+
+##boolean##
+is_age = True
+print(is_age)
+
+## list ##
+name = ["keyz", "python", "kathan"]  ##muteable 
+print(name)
+
+## tuple ##
+name = ("keyz", "python", "kathan")  ##inmutable#
+print(name )
+
+##set 
+number = {"47","64","65","66"}
+print(number)
+
+##dictionary##
+parson = {"name = KEYZ", "age =  22 ", "address =  niko"}
+print(parson)
+
+#####finding types ###
+number= 22 
+print(type(number))
+
+Address = " nikol "
+print(type(Address))
+
+
+number = 22.5 
+print(type(number))
+
+is_age = True
+print(type(is_age))
