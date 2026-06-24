a9b4d129ce78d568e0826fc5247e15809e2cf55d
KEYUR .D.PRAJAPATI <kkeyur136@gmail.com>
2026-06-24 06:33:23 +0000

    own task for fun

---
+++ own task  .py
@@ -0,0 +1,44 @@
+str="keyur prajapati"
+print(str[6:15]) # keyur prajapati
+                #  12345 6 7 8 9 10 11 12 13 14 15
+
+
+age= 16
+if(age>=18):
+    print("can vote and apply for license ")
+
+light = "red"
+if(light =="red"):
+  print("stop")
+elif(light =="green"):
+ print("GOO")
+elif(light=="yellow"):
+ print("look pls")
+
+ #################### end of trafic light model ###################
+
+ ###################### student base onn  mmarkinng ####################
+ mark=int(input("enter the marks : "))
+if(mark>=90):
+    grade = "A"
+elif(mark>=80 and mark<90):
+    grade="B"
+elif(mark>=70 and mark<80):
+    grade="C"
+else:
+    grade="D"
+
+print("grade of the student ->",grade)   
+
+
+#2
+
+number=int(input("enter the number :"))
+if(number%2==0):
+    print("even")
+else:
+    print("odd")  
+
+    #3
+
+    
